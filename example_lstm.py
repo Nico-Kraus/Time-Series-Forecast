c8b7d8c7804586a3fdc01e641b909782c9f57c38
Nico-Kraus <nicokrauscoaching@gmail.com>
2023-11-14 19:40:34 +0100

    refactor

--- example_lstm.py
+++ example_lstm.py
@@ -1,16 +1,14 @@
 from data.data import Data
 from trainer.trainer import Trainer
+from plotting.plot import plot_pred
 from utils import get_params
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 
 trainer_params = get_params("lstm_params.yaml")
 lookback = trainer_params["lookback"]
 
 size = 1000
-seed = 42
-config = {"piecewise_linear": {"num_seg": 10}}
+seed = 41
+config = {"multi_sinusoidal": {"num_sin": 3}}
 
 data = Data(size=size, seed=seed, config=config, lookback=lookback)
 train_df, val_df, test_df = data.get(split=(0.8, 0.1, 0.1))
@@ -19,20 +17,12 @@ trainer = Trainer(**trainer_params)
 train_loss, val_loss, val_pred = trainer.train(train_df, val_df)
 test_loss, test_pred = trainer.test(test_df)
 
-train_df = train_df[lookback + 1 :].rename(columns={train_df.columns[0]: "train"})
-val_df = val_df[lookback + 1 :].assign(pred=val_pred)
-val_df = val_df.set_axis(["val", "val_pred"], axis=1)
-test_df = test_df[lookback + 1 :].assign(pred=test_pred)
-test_df = test_df.set_axis(["test", "test_pred"], axis=1)
-final = pd.concat([train_df, val_df, test_df], ignore_index=True, sort=False)
-
-
-# sns.set_style("darkgrid")
-plt.figure(figsize=(12, 7))
-sns.lineplot(x=final.index, y=final["train"], label="real train")
-sns.lineplot(x=final.index, y=final["val"], label="real val")
-sns.lineplot(x=final.index, y=final["val_pred"], label="pred val")
-sns.lineplot(x=final.index, y=final["test"], label="real test")
-sns.lineplot(x=final.index, y=final["test_pred"], label="pred test")
-plt.legend(loc="upper left")
-plt.show()
+plot_pred(
+    train_df=train_df,
+    val_df=val_df,
+    val_pred=val_pred,
+    test_df=test_df,
+    test_pred=test_pred,
+    lookback=lookback,
+    name="plot",
+)
--- plotting/plot.py
+++ plotting/plot.py
@@ -19,6 +19,56 @@ def plot_time_series(df, name):
     plt.savefig(Path(PATH, "out/examples", f"{name}.png"))
 
 
+def plot_pred(train_df, val_df, val_pred, test_df, test_pred, lookback, name):
+    train_df = train_df[lookback + 1 :].rename(columns={train_df.columns[0]: "train"})
+    val_df = val_df[lookback + 1 :].assign(pred=val_pred)
+    val_df = val_df.set_axis(["val", "val_pred"], axis=1)
+    test_df = test_df[lookback + 1 :].assign(pred=test_pred)
+    test_df = test_df.set_axis(["test", "test_pred"], axis=1)
+    final = pd.concat([train_df, val_df, test_df], ignore_index=True, sort=False)
+
+    sns.set_style("darkgrid")
+    plt.figure(figsize=(12, 7))
+    sns.lineplot(
+        x=final.index,
+        y=final["train"],
+        label="real train",
+        color=(0, 0, 0.8, 0.5),
+        linewidth=1,
+    )
+    sns.lineplot(
+        x=final.index,
+        y=final["val"],
+        label="real val",
+        color=(0, 0, 0.8, 0.5),
+        linewidth=1,
+    )
+    sns.lineplot(
+        x=final.index,
+        y=final["val_pred"],
+        label="pred val",
+        color=(0, 0.8, 0, 0.5),
+        linewidth=1,
+    )
+    sns.lineplot(
+        x=final.index,
+        y=final["test"],
+        label="real test",
+        color=(0, 0, 0.8, 0.5),
+        linewidth=1,
+    )
+    sns.lineplot(
+        x=final.index,
+        y=final["test_pred"],
+        label="pred test",
+        color=(0.8, 0, 0, 0.5),
+        linewidth=1,
+    )
+    plt.legend(loc="upper left")
+    create_dir(Path(PATH, "out/predictions"))
+    plt.savefig(Path(PATH, "out/predictions", f"{name}.png"), dpi=600)
+
+
 def plot_linear_regressions(results, name):
     x_values = list(results.keys())
     y1_values = scale([res["entropy"] for res in results.values()])
@@ -45,7 +95,7 @@ def plot_linear_regressions(results, name):
         ax.set_ylabel("Value")
         ax.set_title("Regression Plots")
         ax.legend()
-
+        create_dir(Path(PATH, "out/results"))
         plt.savefig(Path(PATH, "out/results", f"{name}.png"))
 
 
@@ -60,7 +110,7 @@ def plot_two_graphs(results, name):
     plt.title("Line Plot of the Results Dictionary")
     plt.xlabel("Index")
     plt.ylabel("Blue is Value, Orange is Loss")
-
+    create_dir(Path(PATH, "out/results"))
     plt.savefig(Path(PATH, "out/results", f"{name}.png"))
 
 
@@ -73,4 +123,5 @@ def plot_linear_regression(df, name):
         sns.set_theme()
         g = sns.lmplot(data=df, x=x_name, y=y_name, height=5)
         g.set_axis_labels(x_name, y_name)
+        create_dir(Path(PATH, "out/results"))
         plt.savefig(Path(PATH, "out/results", f"{name}.png"))
